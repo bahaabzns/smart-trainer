241b8e0157ea83535c128f39a7f20ef9f802e539
bahaabzns <bahaa.bzns@gmail.com>
2024-02-21 00:44:27 +0200

    create account and user model

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import AddNewUser from "./pages/AddNewUser";
 import EditUser from "./pages/EditUser";
 import Users from "./pages/Users";
 import Header from "./components/Header";
+import AddNewAccount from "./pages/AddNewAccount";
+import EditAccount from "./pages/EditAccount";
+import Accounts from "./pages/Accounts";
 
 export default function App() {
   return (
@@ -16,8 +19,11 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/sign-in" element={<SignInUser />}/>
         <Route path="/add-new-user" element={<AddNewUser />}/>
-        <Route path="/edit-user-profile" element={<EditUser />}/>
+        <Route path="/edit-user" element={<EditUser />}/>
         <Route path="/users" element={<Users />}/>
+        <Route path="/add-new-account" element={<AddNewAccount />}/>
+        <Route path="/edit-account" element={<EditAccount />}/>
+        <Route path="/accounts" element={<Accounts />}/>
 
       </Routes>
     </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className='bg-slate-300'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+    <div className='bg-slate-300 flex'>
+        <div className='flex gap-4 max-w-6xl mx-auto p-3'>
             <Link to="/">
                 <h1 className='font-bold'>SmarTrainer</h1>
             </Link>
             <ul className='flex gap-4'>
-                <Link to="/">
-                    <li>Home</li>
+                <Link to="/accounts">
+                    <li>Accounts</li>
                 </Link>
                 <Link to="/users">
                     <li>Users</li>
                 </Link>
+            </ul>
+        </div>
+        <div className='flex justify-between max-w-6xl mx-auto p-3'>
+            <ul className='flex gap-4'>
+
                 <Link to="/sign-in">
                     <li>Sign In</li>
                 </Link>
