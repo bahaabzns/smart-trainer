59125d21f81a27e69f0f92568df95a2f1af6581b
bahaabzns <bahaa.bzns@gmail.com>
2024-02-20 22:20:25 +0200

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className='bg-blue-300'>
+    <div className='bg-slate-300'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to="/">
                 <h1 className='font-bold'>SmarTrainer</h1>
