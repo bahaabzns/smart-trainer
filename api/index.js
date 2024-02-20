af1c530c95f3e368439b10259b12fd24c3f05a71
bahaabzns <bahaa.bzns@gmail.com>
2024-02-21 00:04:12 +0200

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('MONGO CONNECTED !!!')
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
+
 app.listen(3000, () => {
     console.log('SERVER IS LIVE ON PORT 3000');
 });
